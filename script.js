6c5459f644884b3fc0622b590c5b990e70fd7cd0
drwwwww <ledanreba@gmail.com>
2025-06-27 20:53:16 -0400

    Initial Commit have already done lots of work but push to gh

---
+++ script.js
@@ -0,0 +1,27 @@
+function submitNote{
+    const note = {
+        title: document.getElementById("newNote").value;
+        content : document.getElementById("newContent").value;
+    }
+
+    fetch("renderlink.com", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(note)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const alert = document.getElementById("alert")
+        alert.classList.add("active");
+
+        setTimeout(() => {
+        alert.classList.remove("active");
+        }, 3000); // disappears after 3 seconds
+    })
+    .catch(err => {
+        console.error("Something went wrong:", err);
+    })
+
+}
